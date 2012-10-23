cf9b13e2522ad202924bfb4fcd4d43f31beedb5a
Frieder Reinhold <reinhold@trigon-media.com>
2012-10-23 17:50:53 +0200

    added whiteboard files

---
+++ apps/tigerkanban/modules/whiteboard/templates/indexNoGroup.php
@@ -0,0 +1,1 @@
+<h2>You need at least one group</h2>
---
+++ apps/tigerkanban/modules/whiteboard/templates/indexSuccess.php
@@ -0,0 +1,32 @@
+<div id="task-frm" title="Task Form">
+    <form>
+        <fieldset>
+            <?php echo $taskform->renderHiddenFields(); ?>
+            <?php echo $taskform ?>
+        </fieldset>
+    </form>
+    <p class="validateTips"></p>
+</div>
+
+
+<div id="dialog-signout" title="Signout?">
+    Do you really want to signout?
+</div>
+
+<div id="dialog-archive" title="Archive?">
+    <p>Do you really want to archive this task? The task will disappear.</p>
+</div>
+
+
+<div id="toolbar">
+    <button id="addtask-btn">Add Task</button>
+    <button id="refresh-btn">Refresh</button>
+    &nbsp;&nbsp;&nbsp;&nbsp;Filter:
+    <div id="filter-radio">
+        <input type="radio" id="filter_all" name="filter-radio" value="all" checked="true" /><label for="filter_all">All</label>
+        <input type="radio" id="filter_me" name="filter-radio" value="me"><label for="filter_me">Me</label>
+    </div>
+</div>
+<div id="main"></div>
+<div class="clearer"></div>
+<div id="archivetarget">Drop tasks here to archive them<ul class="tasklist"></ul></div>
